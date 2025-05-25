4488f31e0cda6c10812ac6445e00b0d1a80a9b93
oheneadj <oheneadjei.dev@gmail.com>
2025-05-25 22:28:49 +0000

    added database migrations

---
+++ database/seeders/PreviousConvictionSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PreviousConvictionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
